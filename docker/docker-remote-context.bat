f79e54c856dddb0aa23b7638bc551df061e4794e
Younes M'RABTI <younesmrabti50@gmail.com>
2025-10-29 17:27:25 +0100

    docker context

--- docker/docker-remote-context.bat
+++ docker/docker-remote-context.bat
@@ -11,8 +11,5 @@ ssh-keygen -f "/root/.ssh/known_hosts" -R "192.168.1.77"
 docker context create remote-msi-windows --docker "host=ssh://root@192.168.1.77"
 docker --context remote-msi-windows ps
 
-$env:CF_ACCESS_CLIENT_ID="77c75d20b45c38ac5e6f5dd59a5fe1b5.access"
-$env:CF_ACCESS_CLIENT_SECRET="6b289bf21eb07ee499e23ce3648e894bf4801c7b518e915df4a33d121e5910d5"
-
-
-cloudflared access tcp --hostname docker.youmrabti.com --service-token-id "77c75d20b45c38ac5e6f5dd59a5fe1b5.access" --service-token-secret "6b289bf21eb07ee499e23ce3648e894bf4801c7b518e915df4a33d121e5910d5"
+ssh-keygen -f "/root/.ssh/known_hosts" -R "192.168.1.77"
+ssh root:MSI_ROOT_2025@192.168.1.77
