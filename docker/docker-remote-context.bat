e2817b63e9c47ff296f53719e12e8f47ed60ec26
Younes M'RABTI <younesmrabti50@gmail.com>
2025-10-29 15:32:10 +0100

    Add remote context creation for Windows in docker-remote-context.bat

--- docker/docker-remote-context.bat
+++ docker/docker-remote-context.bat
@@ -7,6 +7,8 @@ docker --context remote-msi-cloudflared ps
 
 docker context use remote-msi-cloudflared
 
+docker context create remote-msi-windows --docker "host=ssh://root@192.168.1.77"
+docker --context remote-msi-windows ps
 
 $env:CF_ACCESS_CLIENT_ID="77c75d20b45c38ac5e6f5dd59a5fe1b5.access"
 $env:CF_ACCESS_CLIENT_SECRET="6b289bf21eb07ee499e23ce3648e894bf4801c7b518e915df4a33d121e5910d5"
