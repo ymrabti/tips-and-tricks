bfca575b4b642b57689bcd805102dc7f0519e400
Younes M'RABTI <younesmrabti50@gmail.com>
2025-10-30 11:47:47 +0100

    ...

--- docker/docker-remote-context.bat
+++ docker/docker-remote-context.bat
@@ -11,6 +11,10 @@ ssh-keygen -f "/root/.ssh/known_hosts" -R "192.168.1.77"
 docker context create remote-msi-windows --docker "host=ssh://root@192.168.1.77"
 docker --context remote-msi-windows ps
 
+docker run --rm -v majal-ecosystem-win_sql_data:/data -v C:\Users\majal\Dev\backups\pgsql:/host_data alpine sh -c "cp -r /host_data/* /data/"
+
+C:\Users\majal\Dev\backups>
+
 ssh-keygen -f "/root/.ssh/known_hosts" -R "192.168.1.77"
 ssh-keygen -f "%USERPROFILE%\.ssh\known_hosts" -R "192.168.1.77"
 ssh root@192.168.1.77
