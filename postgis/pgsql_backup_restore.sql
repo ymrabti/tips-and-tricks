48df80dc562193859410e960f367dab37ad9f656
Younes M'RABTI <younesmrabti50@gmail.com>
2025-10-30 11:50:59 +0100

    Add PostgreSQL backup and restore script

---
+++ postgis/pgsql_backup_restore.sql
@@ -0,0 +1,5 @@
+
+-- psql -U younes -d SDMB -f 'C:\Users\youmt\Documents\ArcGIS\QGIS\backup-dbgeste.sql'
+
+-- docker cp backup-dbgeste.bak.sql majal_postgres:/backup.sql
+-- docker exec -it majal_postgres bash -c "psql -U StreetAdmin2025 -d EcoGeste -f /backup.sql"
