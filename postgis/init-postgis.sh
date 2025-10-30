18031b9714681d38fdd8f045914f699010488e3b
Younes M'RABTI <younesmrabti50@gmail.com>
2025-10-30 09:45:15 +0100

    .

--- postgis/init-postgis.sh
+++ postgis/init-postgis.sh
@@ -5,3 +5,4 @@ psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<EOSQL
     CREATE EXTENSION IF NOT EXISTS postgis;
     CREATE EXTENSION IF NOT EXISTS postgis_topology;
 EOSQL
+# docker build -t postgis:latest .
