c3a3a6048944e3ea774afff84f1cca760893b678
Younes M'RABTI <younesmrabti50@gmail.com>
2025-10-30 15:25:37 +0100

    ..

--- postgis/pgsql_backup_restore.sql
+++ postgis/pgsql_backup_restore.sql
@@ -3,3 +3,4 @@
 
 -- docker cp backup-dbgeste.bak.sql majal_postgres:/backup.sql
 -- docker exec -it majal_postgres bash -c "psql -U StreetAdmin2025 -d EcoGeste -f /backup.sql"
+-- "C:\Program Files\PostgreSQL\16\pgAdmin 4\runtime\pg_dump.exe" --file "C:\\Users\\youmt\\PROGRA~1\\DBACKUP\\pgsql\\ecogis3.sql" --host "192.168.1.77" --port "5438" --username "StreetAdmin2025" --password="ViewPasswo2025" --format="plain" --section=data --inserts --on-conflict-do-nothing --create --clean --if-exists --quote-all-identifiers "ecogis"
